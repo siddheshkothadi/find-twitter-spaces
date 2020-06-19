8c3d9a63b72765e480b8d16be9644290c534a8e8
siddheshkothadi <realsiddheshko@gmail.com>
2020-06-19 15:35:06 +0530

    commit

--- App.js
+++ App.js
@@ -4,35 +4,38 @@ import { Button, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({navigation }) {
+function HomeScreen({route,navigation }) {
+	let txt="";
+	if(route.params!=null){
+		txt="Previous score: "+route.params.score;
+	}
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button
-          onPress={() => navigation.navigate("Game",{"score": 0,"rand": 9})}
+          onPress={() => navigation.navigate("Game",{"score": 0,"rand1":Math.floor(Math.random() * 10),"rand2":Math.floor(Math.random() *10)})} 
           title="Start Game"
           color="#841584"
         />
+	  <Text>{txt}</Text>
     </View>
   );
 }
 
 function GameScreen({route, navigation }) {
-  const {score, rand} = route.params
-  //console.log(val);
+  const {score, rand1,rand2} = route.params
   const [text, setText] = useState('');
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{padding: 10, fontSize: 60}}>
-          {rand}
-        </Text>
+        <Text style={{padding: 10, fontSize: 60}}>{rand1}+{rand2}</Text>
         <TextInput
+	  keyboardType='numeric'  
          style={{borderColor:'gray',marginBottom:20, textAlign:'center',width: 250,padding: 10}}
-         placeholder="Enter the number you see above"
+         placeholder="Enter the addition"
          onChangeText={text => setText(text)}
          defaultValue={text}
        />
         <Button
-          onPress={text==rand ? () =>navigation.push("Game",{"score":score+1,"rand": Math.floor(Math.random() * 10)}) : () =>navigation.popToTop()}
+          onPress={text==rand1+rand2 ? () =>navigation.push("Game",{"score":score+1,"rand1": Math.floor(Math.random() * 10),"rand2":Math.floor(Math.random()*10)}) : () =>navigation.navigate("Home",{"score":score})}
           title="Submit"
           color="#841584"
         />
@@ -42,21 +45,6 @@ function GameScreen({route, navigation }) {
     </View>
   );
 }
-
-// const [text, setText] = useState('');
-//   return (
-//     <View style={{padding: 10}}>
-//       <TextInput
-//         style={{height: 40}}
-//         placeholder="Type here to translate!"
-//         onChangeText={text => setText(text)}
-//         defaultValue={text}
-//       />
-//       <Text style={{padding: 10, fontSize: 42}}>
-//         {text.split(' ').map((word) => word && '🍕').join(' ')}
-//       </Text>
-//     </View>
-//   );
 
 const Stack = createStackNavigator();
 
